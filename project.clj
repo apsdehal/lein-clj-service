985f3d6b8a56a44c6c88a6205bbf063071b1e3ff
Amanpreet Singh <apsdehal@gmail.com>
2015-07-02 21:14:17 -0700

    Initial project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-service/lein-template "1.0.0"
+  :description "Project template for clojure based HTTP services."
+  :min-lein-version "2.0.0"
+  :eval-in-leiningen true
+  :url "https://github.com/apsdehal/lein-clj-service"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-ancient "0.5.5"]])
