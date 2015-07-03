dca7a9c054c6999365e132b67edc07f007fc041e
Amanpreet Singh <apsdehal@gmail.com>
2015-07-02 22:21:25 -0700

    Add resource template

---
+++ src/leiningen/clj_service/resources.clj
@@ -0,0 +1,15 @@
+(ns {{dot-path}}.{{name}}.resources
+  (:require
+    [ring.util.http-response :as http]
+    [compojure.api.sweet :refer [defroutes* GET*]]
+    [com.climate.compojure.api.meta]))
+
+(defroutes* v1_routes
+  (GET*
+    "/{{name}}"
+    []
+    :new-relic-name "/v1/{{name}}"
+    :summary "Dummy Route to index"
+    :description
+    "<p>Add a description here</p>"
+    (http/ok "OK")))
